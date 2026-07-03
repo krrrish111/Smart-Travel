1e411b74a17791712d00edf6bdb0ff8ff3a66c16
Sanket Agrawal <24bcs034@ietdavv.edu.in>
2026-07-03 22:20:41 +0530

    Import java.sql.DatabaseMetaData to fix compilation

--- src/main/java/com/voyastra/util/SchemaBootstrap.java
+++ src/main/java/com/voyastra/util/SchemaBootstrap.java
@@ -9,6 +9,7 @@ import java.sql.Connection;
 import java.sql.Statement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.DatabaseMetaData;
 import com.voyastra.config.ConfigManager;
 
 /**
